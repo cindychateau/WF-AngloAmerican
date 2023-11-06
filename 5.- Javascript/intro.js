279c24cfb15a130eb76e06a4bae9e0ba2032e23f
Cynthia Castillo <ca.castillo@gmail.com>
2023-11-06 15:38:43 -0600

    Arreglos, Objetos e inicio Pokemon

--- 5.- Javascript/intro.js
+++ 5.- Javascript/intro.js
@@ -132,3 +132,61 @@ while(inicio < final) {
     inicio++;
     final--;
 }
+
+/* ARREGLOS/ARRAY/LISTA */
+var hobbies = ["Leer", "Correr", "Bailar", "Jugar videojuegos", "Tocar el piano"];
+console.log(hobbies[0]);
+hobbies[1] = "Trotar";
+console.log(hobbies);
+
+console.log(hobbies.length); //Tamaño de mi arreglo
+
+var combinado = [
+    1.11,
+    "Texto",
+    false,
+    30,
+    ["otra", "lista", "adentro"]
+];
+
+//let -> Solo funciona en un bloque de codigo
+for(let i=0; i<hobbies.length; i++) { //i += 1
+    console.log(hobbies[i]);
+}
+
+console.log(hobbies[hobbies.length-1]); //Último valor del arreglo
+
+hobbies.push("Ver series"); //Agregamos al final de la lista un valor
+console.log(hobbies);
+
+var nombres = ["Juana de Arco", "Elena de Troya", "Pedro Páramo", "Pablo Picasso"];
+var nombre_quitado = nombres.pop(); //Eliminamos el último elemento del listado
+console.log(nombres);
+console.log(nombre_quitado);
+
+var matriz = [
+    [1, 2, 3, 4, 5], //matriz[0]
+    [6, 7, 8, 9, 10] //matriz[1]
+];
+
+matriz[1][2] = 5;
+console.log(matriz);
+
+//[] -> Arreglos/Listas
+//{} -> Objetos
+
+/* OBJETOS */
+var estudiante = {
+    "id": 12345,
+    "nombre": "Elena",
+    "apellido": "De Troya",
+    "edad": 25,
+    "hobbies": ["Leer", "Bailar", "Ver pelis"]
+}
+
+console.log(estudiante["nombre"]);
+console.log(estudiante.apellido);
+estudiante.apellido = "De Arco";
+console.log(estudiante);
+estudiante["curso"] = "Python";
+console.log(estudiante);
